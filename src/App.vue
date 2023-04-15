add96d75e3e67528ae8e55fb5c49b0d222c94d29
zyw <78946068+SleepinWei@users.noreply.github.com>
2023-04-15 17:07:21 +0800

    feat : add header

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,53 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <Header v-if="state.isShowHeader"></Header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header.vue"
+import { onMounted, reactive } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+  setup() {
+    const state = reactive({
+      isShowHeader: true,
+    });
+    const route = useRoute();
 
-nav {
+    const routeChange = (val, oldVal) => {
+      // const referrer= document.getElementById("referrer");
+      if (val.path === "/") {
+        state.isShowHeader = true;
+        // referrer.setAttribute("content", "always");
+      }
+    };
+
+    onMounted(() => {
+      routeChange(route, route);
+    })
+
+
+    return {
+      state,
+      routeChange
+    };
+  }
+}
+</script>
+
+<style>
+.outer-header{
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  height: 50px;
+  min-width: 1300px;
+  box-shadow: 0px 4px 4px 1px rgba(0, 0, 0, .2);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+    <el-row align="middle" class="header">
+        <el-col :span="4" :offset="1">
+            <router-link class="link" to="/">
+                    Exhibition Hub
+            </router-link>
+        </el-col>
+        <el-col :span="4">
+            <el-row :gutter="2">
+            <el-col :span="8">
+                <router-link class="link" to="/">
+                        首页
+                </router-link>
+            </el-col>
+            <el-col :span="8">
+                <router-link class="link" to="/search">
+                        分类
+                </router-link>
+            </el-col>
+            </el-row>
+        </el-col>
+
+        <!-- 搜索栏 -->
+        <el-col :span="3">
+            <!-- 空占位 -->
+        </el-col>
+        <el-col :span="8">
+            <el-input
+                v-model="input" 
+                size="large"
+                placeholder="Input"
+            >
+            <template #prepend>
+                <el-button>
+                    <el-icon>
+                        <Search />         
+                    </el-icon>
+                </el-button>
+            </template>
+            </el-input>
+        </el-col>
+
+        <!-- 用户头像 -->
+        <el-col :span="4">
+            <el-row justify="center">
+                用户名
+            </el-row>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+// import { ref } from 'vue'
+
+</script>
+
+<style>
+.header {
+    width: 100%;
+    height: 50px;
+}
+.link{
+    text-decoration: none;
+}
+</style>
