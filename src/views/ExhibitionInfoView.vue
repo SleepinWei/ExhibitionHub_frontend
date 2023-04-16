78eff03d72d49e7500e797c10e3690662a6b73bd
SleepinWei <zyw0241@outlook.com>
2023-04-16 15:09:06 +0800

    feat :  update design of info page

--- src/views/ExhibitionInfoView.vue
+++ src/views/ExhibitionInfoView.vue
@@ -1,36 +1,67 @@
 <template>
 <el-row>
-    <el-col :span="9">
-        <div class="poster-image">
-            <el-image style="width: 300px;height: 300px;" :src="poster_url" fit="contain">
-            </el-image>
-        </div>
+    <el-col :span="18">
+    <el-row class="basic_info">
+        <el-col :span="9">
+            <el-row justify="center" class="poster-image">
+                <el-image style="width: 250px;height: 300px;" :src="poster_url" fit="contain">
+                </el-image>
+            </el-row>
+        </el-col>
+        <el-col :span="13" :offset="0">
+            <el-row class="sub_info">
+                <h2>
+                    {{name}}
+                </h2>
+            </el-row>
+            <el-row class="sub_info">
+                时间：{{begin_time}} - {{ end_time }}
+            </el-row>
+            <el-row class="sub_info">
+                主办方：{{organizer }}
+            </el-row>
+            <el-row class="sub_info">
+                票价: {{tickets}}
+            </el-row>
+            <el-row class="sub_info">
+                官方链接: 
+                <a :href="link">
+                    {{link}}
+                </a>
+            </el-row>
+            <el-row class="sub_info">
+                标签: 
+                <el-tag v-for="tag in tags"> tag </el-tag>
+            </el-row>
+        </el-col>
+
+    </el-row>
+
+    <el-row class="long_intro">
+        <h2>详细介绍</h2>
+    </el-row>
+    <el-row class="long_intro">
+        <!-- {{ intro }} -->
+        <div v-html="long_intros"></div>
+    </el-row>
     </el-col>
-    <el-col :span="13" :offset="2">
+    <el-col :span="6">
+        <!-- 推荐信息 -->
         <el-row>
-            name
+            <h2>
+                推荐展览
+            </h2>
         </el-row>
         <el-row>
-            time
-        </el-row>
-        <el-row>
-            organizer 
-        </el-row>
-        <el-row>
-            tickets
-        </el-row>
-        <el-row>
-            link
-        </el-row>
-        <el-row>
-            tags
+            <ul>
+                <li v-for="recommend in recommends">
+                    {{recommend}}
+                </li>
+            </ul>
         </el-row>
     </el-col>
 </el-row>
 
-<el-row>
-    introductions
-</el-row>
 </template>
 
 <script>
@@ -38,15 +69,50 @@ export default {
     data() {
         return {
             exId: this.$route.params.exId,
-            poster_url: "/src/assets/posters/saber.png"
+            poster_url: "/src/assets/posters/saber.png",
+            name: "Exhibition 1",
+            begin_time: "2001-01-01",
+            end_time: "2001-02-02",
+            organizer: "og1",
+            tickets: "2000RMB",
+            link: "https://bilibili.com",
+            tags: ["tag1", "tag2"],
+            intro: "some long introssssss\nssssssssssssssssssssssssssss\nssssssssssssssssssssss\
+            ssssssssssssssssssssssssssssss",
+            recommends:["ex1","ex2","ex3"]
         }
     },
     mounted() {
         console.log(this.exId);
+    },
+    computed: {
+        long_intros() {
+            var arr = this.intro.split("\n");
+            var result = ""; 
+            arr.forEach((value, index, array) => {
+                result += `<p>${value}</p>`
+            });
+            return result;
+        }
     }
 }
 
 </script>
 
 <style>
+.basic_info{
+    padding-top:30px;
+}
+
+.el-tag{
+    margin-left: 5px;
+}
+
+.sub_info{
+    margin-top: 5px;
+}
+
+.long_intro{
+    padding-left: 50px;
+}
 </style>
