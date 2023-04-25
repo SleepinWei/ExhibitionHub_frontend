a7a20ca0ff676dd7ea9c728dea8379799a82482e
zyw <78946068+SleepinWei@users.noreply.github.com>
2023-04-25 16:37:50 +0800

    feat : fix various bugs

--- src/views/AlterInfoView.vue
+++ src/views/AlterInfoView.vue
@@ -34,10 +34,10 @@
       </el-form-item>
       <el-form-item label="活动时间">
         <el-col :span="11">
-          <el-date-picker
+          <el-time-picker
             v-model="form.begin_time"
-            type="date"
             placeholder="选择开始时间"
+            value-format="HH:mm:ss"
             style="width: 100%"
           />
         </el-col>
@@ -48,6 +48,7 @@
           <el-time-picker
             v-model="form.end_time"
             placeholder="选择结束时间"
+            value-format="HH:mm:ss"
             style="width: 100%"
           />
         </el-col>
@@ -117,6 +118,9 @@
   import axios from "axios"
 import { useRoute, useRouter } from 'vue-router'
 
+const router = useRouter();
+const route = useRoute();
+
 const imageUrl = ref('')
   // do not use same name with ref
   const form = ref({
@@ -160,8 +164,11 @@ const imageUrl = ref('')
         // }
         
         data: form.value,
-    })
-    .then();
+    }).then(
+        (response) => {
+          router.push(`/exhibition/${route.params.exId}`)
+      }
+    );
   }
 
   // const checked_sports = ref(false)
@@ -228,8 +235,6 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
       }
     }
 
-  const route = useRoute();
-
 onMounted(() => {
     if (route.params.exId == null)
     return;
@@ -242,6 +247,7 @@ onMounted(() => {
         }
       }
       ).then((response) => {
+        console.log(response.data);
         form.value = response.data;
     });
 });
