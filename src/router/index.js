1dd0cabdd3b818aff1453c0e17db600fcf1317ce
SleepinWei <zyw0241@outlook.com>
2023-04-29 16:01:20 +0800

    feat : 添加审查页面

--- src/router/index.js
+++ src/router/index.js
@@ -84,6 +84,10 @@ const router = createRouter({
     {
       path: "/search",
       component: ()=> import("@/views/SearchView.vue")
+    },
+    {
+      path: "/audit",
+      component: ()=> import("@/views/AuditView.vue") 
     }
   ]
 })
---
+++ src/views/AuditView.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-row>
+        <el-col :span="12" :offset="2">
+        <h1>审核信息</h1>
+        </el-col>
+    </el-row>
+    <el-row justify="center">
+    <el-table :data="tableData" stripe style="width: 80%" @row-dblclick="dbclick">
+        <el-table-column prop="name" label="Exhibition Name"/>
+        <el-table-column prop="last_edit" label="Last Edit"/>
+    </el-table>
+    </el-row>
+</template>
+
+<script>
+import { ClickOutside } from 'element-plus';
+
+
+export default {
+    data() {
+        return {
+            tableData: [
+                {
+                    name : "Tom",
+                    last_edit : "2002-04-01 23:00:00",
+                    exId : 2
+                },
+                {
+                    name : "Jerry",
+                    last_edit : "2002-04-02 21:00:00",
+                    exId: 1 
+                }
+            ]
+        }
+    },
+    methods: {
+        dbclick(row,col,event){
+            var exId = row.exId;
+            this.$router.push(`/exhibition/${exId}`)
+        }
+
+    }
+}
+
+</script>
+
+<style>
+
+</style>
