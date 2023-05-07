489615ab46029a49a48fbe3510fb7baa7cf0d0fc
Claireliu0912 <83447246+Claireliu0912@users.noreply.github.com>
2023-05-07 20:40:24 +0800

    Merge branch 'shareExhibiton' into schedulemanage

--- src/components/ImageDownloadItem.vue
+++ src/components/ImageDownloadItem.vue
@@ -2,10 +2,6 @@
 import PosterWithQRcode from './PosterWithQRcode.vue'
 </script>
 
-<script setup>
-import PosterWithQRcode from './PosterWithQRcode.vue'
-</script>
-
 <template>
     <div>
       <PosterWithQRcode 
@@ -28,6 +24,16 @@ import PosterWithQRcode from './PosterWithQRcode.vue'
       poster_url: {
         type: String
       },
+      tolink: {
+        type: String
+      },
+    },
+    data(){
+      return{
+        downloadurl:'',
+        filePath:'/src/assets/posters/',
+        filename:'saber.png',
+      };     
     },
     methods: {
       getPosterUrl(url){
@@ -35,12 +41,42 @@ import PosterWithQRcode from './PosterWithQRcode.vue'
         this.downloadurl=url;
       },
       downloadImage() {
-        const link = document.createElement('a');
-        link.href = this.poster_url;
-        link.download = 'image.jpg';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        // axios.get(this.downloadurl, {responseType: 'blob'})
+        // .then(response => {
+        //   console.log("yesyes")
+        //   const blob = response.data
+        //   saveAs(blob, 'poster.png')
+        // })
+        // console.log("IMAGEDOWN",this.downloadurl)
+        // const link = document.createElement("a");
+        // link.href = this.downloadurl;
+        // link.setAttribute('download', 'poster.png')
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
+        console.log("Download!!!!!")
+        this.$axios.get("http://localhost:8080/downLoadFile/",{
+              params: {
+                path: this.filePath,
+                name: this.fileName
+              },
+              responseType: 'blob'
+            }).then(res => {
+              const blob = new Blob([res.data]);
+              const fileName = res.headers["content-disposition"].split(";")[1].split("filename=")[1];
+              if ('download' in document.createElement("a")) {
+                const link = document.createElement("a");
+                link.download = fileName;
+                link.style.display = 'none';
+                link.href = URL.createObjectURL(blob);
+                document.body.appendChild(link);
+                link.click();
+                URL.revokeObjectURL(link.href);
+                document.body.removeChild(link);
+              } else {
+                navigator.msSaveBlob(blob, fileName);
+              }
+			      })
       },
       close() {
         this.$emit('close');
