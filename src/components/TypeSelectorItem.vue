06f6b4f41004fef2583b96f03515cf7fcc6f1bb7
vvvi <wangyxin517@163.com>
2023-05-09 16:27:51 +0800

    feat: 添加根据后端内容展示主办方和标签的内容

--- src/components/TypeSelectorItem.vue
+++ src/components/TypeSelectorItem.vue
@@ -1,48 +1,60 @@
 <template>
     <div class="typeSelecotr">
-        <el-checkbox-group 
-        v-model="value" 
-        size="small"
-        @change="getType"
-        >
+        <el-checkbox-group v-model="value" size="small" @change="getType">
             <el-checkbox-button v-for="i in types" :key="i.id" :label="i.id">
                 {{ i.name }}
             </el-checkbox-button>
         </el-checkbox-group>
     </div>
-    
 </template>
   
-  <script>
-  export default {
-        data() {
-            return{
-                value:[],
-                types: [
-                    {id:1,name:'美术'},
-                    {id:2,name:'书法'},
-                    {id:3,name:'美食'},
-                    {id:4,name:'时尚'},
-                    {id:5,name:'现代'},
-                    {id:6,name:'古典'},
-                ],
-                str:""
-            } 
-        },        
-        methods:{
-            getType(data){     
-                this.$emit('selectType', data);
-            },
-    }
-  }
-  </script>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            value: [],
+            types: [
+                // { id: 1, name: '美术' },
+                // { id: 2, name: '书法' },
+                // { id: 3, name: '美食' },
+                // { id: 4, name: '时尚' },
+                // { id: 5, name: '现代' },
+                // { id: 6, name: '古典' },
+            ],
+            str: ""
+        }
+    },
+    methods: {
+        getType(data) {
+            this.$emit('selectType', data);
+        },
+        getTags() {
+            axios.get(`/tagSelection/getAllTag`)
+                .then(
+                    (response) => {
+                        console.log(response.data);
+                        this.types = [];
+                        var data = response.data;
+                        for (var i = 0; i < data.length; i += 1) {
+                            this.types.push({ id: data[i].id, name: data[i].name });
+                        }
+                        console.log(this.types)
+                    }
+                );
+
+        },
+    },
+    mounted() {
+        this.getTags();
+    },
+}
+</script>
   
-  <style scoped>
-  
+<style scoped>
 .typeSelecotr {
-    margin-top:15px;
-    margin-bottom:20px;
+    margin-top: 15px;
+    margin-bottom: 20px;
     flex: 1;
-  }
- 
-  </style>
+}
+</style>
