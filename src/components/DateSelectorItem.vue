9d7cd38d71b5d6a6f206ad6d8af172d73cb60ec7
vvvi <wangyxin517@163.com>
2023-05-13 19:52:46 +0800

    feat:搜索标签选择

--- src/components/DateSelectorItem.vue
+++ src/components/DateSelectorItem.vue
@@ -1,63 +1,53 @@
 <template>
-    <div class="demo-date-picker">
-      <div class="block hvr-shadow">
-        <el-date-picker
-          v-model="value"
-          type="daterange"
-          unlink-panels
-          range-separator="-"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-          value-format="YYYY-MM-DD"
-          @change="getSelectDate"
-        />
-      </div>
+  <div class="demo-date-picker">
+    <div class="block hvr-shadow">
+      <el-date-picker v-model="value" type="daterange" unlink-panels range-separator="-" start-placeholder="开始日期"
+        end-placeholder="结束日期" value-format="YYYY-MM-DD" @change="getSelectDate" />
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-        data() {
-            return{
-                value:new Date(),
-                startTime:'1900-01-01',
-                endTime:'2100-12-31',
-            } 
-        },        
-        methods:{
-            getSelectDate(data){
-                this.startTime=data[0]
-                this.endTime=data[1]
-                console.log(this.startTime)
-                console.log(this.endTime)
-                this.$emit('change', this.startTime,this.endTime);
-            },
-        },
-        beforeMount() {
-          //this.loadContent()
-        }
-
+<script>
+export default {
+  data() {
+    return {
+      value: new Date(),
+      startTime: '1900-01-01',
+      endTime: '2100-12-31',
     }
-  </script>
-  
-  <style scoped>
-  .demo-date-picker {
-    display: flex;
-    width: 100%;
-    padding: 0;
-    flex-wrap: wrap;
-  }
-  
-  .demo-date-picker .block {
-    text-align: center;
-    flex: 1;
-    width: 100%;
-  }
-  
-  .demo-date-picker .block:last-child {
-    border-right: none;
+  },
+  methods: {
+    getSelectDate(data) {
+      this.startTime = data[0]
+      this.endTime = data[1]
+      console.log(this.startTime)
+      console.log(this.endTime)
+      this.$emit('change', this.startTime, this.endTime);
+    },
+  },
+  beforeMount() {
+    //this.loadContent()
   }
 
+}
+</script>
+  
+<style scoped>
+.demo-date-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
 
-  </style>
+.demo-date-picker .block {
+  text-align: center;
+  flex: 1;
+  width: 100%;
+}
+
+.demo-date-picker .block:last-child {
+  border-right: none;
+}
+</style>
   
--- src/components/MuseumSelectorItem.vue
+++ src/components/MuseumSelectorItem.vue
@@ -1,57 +1,63 @@
 <template>
     <div class="museumSelecotr">
         <div class="block">
-            <el-select 
-            class="hvr-shadow"
-            v-model="value" 
-            filterable
-            placeholder="---------------不限---------------"
-            @change="handleChange"
-            >
-                <el-option
-                v-for="item in museumOptions"
-                :key="item.id"
-                :label="item.label"
-                :value="item.label"
-                />
+            <el-select class="hvr-shadow" v-model="value" filterable placeholder="---------------不限---------------"
+                @change="handleChange">
+                <el-option v-for="item in museumOptions" :key="item.id" :label="item.label" :value="item.label" />
             </el-select>
         </div>
     </div>
-    
 </template>
   
-  <script>
-  export default {
-        data() {
-            return{
-                value:'',
-                option:0,
-                museumOptions: [
-                    {id:0,label:'---------------不限---------------'},
-                    {id:1,label:'上海博物馆'},
-                    {id:2,label:'上海科技馆'},
-                    {id:3,label:'上海自然博物馆'},
-                ],
-            } 
-        },        
-        methods:{
-            handleChange(data) {
-                this.option=data
-                console.log(this.option)
-                this.$emit('change', this.option);
-            }
-    }
-  }
-  </script>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            value: '',
+            option: 0,
+            museumOptions: [
+                { id: 0, label: '---------------不限---------------' },
+                // {id:1,label:'上海博物馆'},
+                // {id:2,label:'上海科技馆'},
+                // {id:3,label:'上海自然博物馆'},
+            ],
+        }
+    },
+    methods: {
+        handleChange(data) {
+            this.option = data
+            console.log(this.option)
+            this.$emit('change', this.option);
+        },
+        getAllOrganizer() {
+            axios.get(`/tagSelection/getAllOrganizer`)
+                .then(
+                    (response) => {
+                        console.log(response.data);
+                        this.museumOptions = [{ id: 0, label: '---------------不限---------------' },];
+                        var data = response.data;
+                        for (var i = 0; i < data.length; i += 1) {
+                            this.museumOptions.push({ id: i + 1, label: data[i] });
+                        }
+                        console.log(this.museumOptions)
+                    }
+                );
+        }
+    },
+    mounted() {
+        this.getAllOrganizer();
+    },
+}
+</script>
   
-  <style scoped>
-
+<style scoped>
 .museumSelecotr {
     display: flex;
-  }
+}
+
 .block {
     padding: 2% 0;
     text-align: center;
-  }
- 
-  </style>
+}
+</style>
